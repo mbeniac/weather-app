665d584e0d8ae9cd529076854dd981f6af577996
mbeniac <mandybeniac@gmail.com>
2022-02-13 13:13:27 -0600

    java

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = response.data.main.temp;
+}
 
+let apiKey = "c3be0ab72af9c5b189f134b093f111bc";
+let apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?q={Chicago}&appid=${apiKey}&units=metrics";
+
+axios.get(apiUrl).then(displayTemperature);
