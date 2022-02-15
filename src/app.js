2d3f766db4dbcbd2b9991adc6e8b711a9dd27507
mbeniac <mandybeniac@gmail.com>
2022-02-14 19:27:02 -0600

    humidity

--- src/app.js
+++ src/app.js
@@ -2,9 +2,11 @@ function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let conditionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
   conditionElement.innerHTML = response.data.weather[0].description;
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
+  humidityElement.innerHTML = response.data.main.humidity;
 }
 
 let apiKey = "c3be0ab72af9c5b189f134b093f111bc";
