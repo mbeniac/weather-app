01a70ff84c7491d3d9ebee6ea0442cbde36a96bc
mbeniac <mandybeniac@gmail.com>
2022-02-14 18:58:55 -0600

    exact temp

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = response.data.main.temp;
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
 }
 
 let apiKey = "c3be0ab72af9c5b189f134b093f111bc";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Chicago&appid=${apiKey}&units=metrics`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=chicago&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
