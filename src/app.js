6275ef9cc169b62b356f0c04f407e68142c6557a
mbeniac <mandybeniac@gmail.com>
2022-02-14 18:07:22 -0600

    api url

--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ function displayTemperature(response) {
 }
 
 let apiKey = "c3be0ab72af9c5b189f134b093f111bc";
-let apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?q={Chicago}&appid=${apiKey}&units=metrics";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Chicago&appid=${apiKey}&units=metrics`;
 
 axios.get(apiUrl).then(displayTemperature);
